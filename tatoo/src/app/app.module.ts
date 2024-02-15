46b61345c6130a0bc4da1b37caee70cda056b509
DirtdiverIV <davidgomez.seg@gmail.com>
2024-02-15 17:18:36 +0100

    Installed boostrap,jquery and popper.js

--- tatoo/src/app/app.module.ts
+++ tatoo/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    GalleryComponent,
+    ArtistComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
